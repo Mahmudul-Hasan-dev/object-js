4f07bf13b60b42319e197712136fee6de6bc5291
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-07 00:55:35 +0600

    object done

---
+++ object.js
@@ -0,0 +1,43 @@
+//object declaration
+var student = {
+    id: 01,
+    name: 'nahid',
+    class: 12,
+    marks: 89
+}
+
+//object print
+console.log(student);
+
+//single property from object
+console.log(student.name);
+
+//object declaration
+var mobile = {
+    brand: 'apple',
+    model: '6s',
+    price: 30000,
+    camera: '25MP'
+}
+//object print
+console.log(mobile);
+
+//single property from object
+console.log(mobile.brand);
+
+//object declaration
+var myComputer = {
+    brand: 'apple',
+    model: 'macbook pro',
+    price: 250000,
+    storage: '512gb'
+}
+//object print
+console.log(myComputer);
+
+//single property from object
+console.log(myComputer.model);
+
+//set property value of object
+myComputer.storage = '1tb';
+console.log(myComputer);
